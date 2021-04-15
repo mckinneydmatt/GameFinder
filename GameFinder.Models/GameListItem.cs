285e95e54d264cb4bb5442237be1ab213cc70fc7
mckinneydmatt <mckinneydmatt@gmail.com>
2021-04-15 15:23:13 -0400

    ABC order and get by title complete

---
+++ GameFinder.Models/GameListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Models
+{
+    public class GameListItem
+    {
+
+        public int ID { get; set; }
+        public string GameTitle { get; set; }
+
+        public bool IsSinglePlayer { get; set; }
+     
+
+    }
+}
+
